c8fc29e3d5649c9db6d5e6d7aad832d7ba358bfd
johnshaughnessy <johnfshaughnessy@gmail.com>
2018-09-13 18:26:01 -0700

    WIP userinput

--- src/react-components/ui-root.js
+++ src/react-components/ui-root.js
@@ -163,7 +163,7 @@ class UIRoot extends Component {
   };
 
   spawnPen = () => {
-    this.props.scene.emit("spawn_pen");
+    this.props.scene.emit("penButtonPressed");
   };
 
   handleStartEntry = () => {
